0c03402ac3dcd0a76699f4a5c689dbc002cdd9a8
Loreto Cid <lore.cid921@gmail.com>
2025-03-03 00:12:24 -0300

    se agrega carpeta usuario

---
+++ practicas/5_usuario/usuario.py
@@ -0,0 +1,10 @@
+class Usuario:
+    def __init__(self, nombre, apellido, email):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.email = email
+        self.limite_credito = 30000
+        self.saldo_pagar = 0
+
+    def hacer_compra(self, monto):  #recibe como argumento el monto de la compra
+        self.saldo_pagar += monto   #el saldo a pagar del usuario aumenta en la cantidad del valor recibido
